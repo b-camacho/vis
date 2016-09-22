6b11cd496c7e2a54af34b463412184bc5b880bd2
nopony <bc.biran@gamil.com>
2016-09-23 01:20:06 +0200

    changed port to standard http

--- server.js
+++ server.js
@@ -4,40 +4,14 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-var busboy = require('connect-busboy');
 var fs = require('fs');
-/*
-var publicDirectories = [
-    'css',
-    'html',
-    'fonts',
-    'js'
-];
-publicDirectories.forEach(function(directory) {
-    app.use(express.static('/' + directory));
-});*/
+var config = JSON.parse(fs.readFileSync('config.json').toString());
 app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
-app.use(busboy());
-/*app.use(express.json());
-app.use(express.urlencoded());*/
 
-app.get('/', function (req, res) {
-    res.send('Hello World!');
-});
-app.post('/scholar-csv', function (req, res) {
-
-    console.log(req.body)
-    /*req.busboy.on('file', function(fieldname, file, filename) {
-        console.log('Started uploading ' + filename);
-        console.log(file);
-    })*/
-
-    res.send('success');
-});
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+app.listen(config.port || 3000, function () {
+    console.log('Example app listening on port ' + (config.port || 3000));
 });
