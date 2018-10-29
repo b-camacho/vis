1bf8f9835a83d0a777f4d7fe255c38ce8bcab7e6
Brian Camacho <hello@briancamacho.me>
2018-10-29 23:34:46 +0000

    new domains, version bump

--- sources/js/domains.js
+++ sources/js/domains.js
@@ -1,9 +1,10 @@
 var DOMAINS = [
-	{topic: "Dziedzina nauk humanistycznych", weight: 0.268, hue: "#868686", amount: 0},
-	{topic: "Dziedzina nauk społecznych", weight: 0.230, hue: "#1D1DBB", amount: 0},
-	{topic: "Dziedzina nauk medycznych i o zdrowiu", weight: 0.09, hue: "#D81D1D", amount: 0},
-	{topic: "Dziedzina nauk inżynieryjnych i technicznych", weight: 0.055, hue: "#E92DCC", amount: 0},
-	{topic: "Dziedzina nauk ścisłych i przyrodniczych", weight: 0.301, hue: "#6EE9C4", amount: 0},
-	{topic: "Dziedzina nauk rolniczych", weight: 0.034, hue: "#39B918", amount: 0},
-	{topic: "Dziedzina sztuki", weight: 0.022, hue: "#F8CE1A", amount: 0}
+	{topic: "Dziedzina nauk humanistycznych", weight: 0.2346, hue: "#868686", amount: 0},
+	{topic: "Dziedzina nauk teologicznych", weight: 0.0367, hue: "#0da781", amount: 0},
+	{topic: "Dziedzina nauk społecznych", weight: 0.233, hue: "#1D1DBB", amount: 0},
+	{topic: "Dziedzina nauk medycznych i o zdrowiu", weight: 0.0913, hue: "#D81D1D", amount: 0},
+	{topic: "Dziedzina nauk inżynieryjnych i technicznych", weight: 0.054, hue: "#E92DCC", amount: 0},
+	{topic: "Dziedzina nauk ścisłych i przyrodniczych", weight: 0.3054, hue: "#6EE9C4", amount: 0},
+	{topic: "Dziedzina nauk rolniczych", weight: 0.0228, hue: "#39B918", amount: 0},
+	{topic: "Dziedzina sztuki", weight: 0.0222, hue: "#F8CE1A", amount: 0}
 ]
