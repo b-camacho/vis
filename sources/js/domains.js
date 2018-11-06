d9baa6976567c3ae69fe4e29334d342b866be21d
Brian Camacho <hello@briancamacho.me>
2018-11-06 15:55:54 +0000

    tweak colour

--- sources/js/domains.js
+++ sources/js/domains.js
@@ -1,6 +1,6 @@
 var DOMAINS = [
 	{topic: "Dziedzina nauk humanistycznych", weight: 0.2346, hue: "#868686", amount: 0},
-	{topic: "Dziedzina nauk teologicznych", weight: 0.0367, hue: "#0da781", amount: 0},
+	{topic: "Dziedzina nauk teologicznych", weight: 0.0367, hue: "#05a7b8", amount: 0},
 	{topic: "Dziedzina nauk społecznych", weight: 0.233, hue: "#1D1DBB", amount: 0},
 	{topic: "Dziedzina nauk medycznych i o zdrowiu", weight: 0.0913, hue: "#D81D1D", amount: 0},
 	{topic: "Dziedzina nauk inżynieryjnych i technicznych", weight: 0.054, hue: "#E92DCC", amount: 0},
