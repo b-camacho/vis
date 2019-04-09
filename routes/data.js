159dc0a6f2a1f20efc5059684fbb63cd2ad76b5a
Brian Camacho <hello@briancamacho.me>
2019-04-09 20:19:08 +0100

    add restful data route

--- routes/data.js
+++ routes/data.js
@@ -3,7 +3,6 @@ var router = express.Router();
 var m = require('../models');
 
 router.get('/department/:name', function (req, res, next) {
-    // m.IDepartment.find().then(console.log)
     m.Department.findOne({shortName: req.params.name})
         .then(dept => {
             res.json(dept)
@@ -15,5 +14,16 @@ router.get('/department/:name', function (req, res, next) {
         })
 });
 
+router.get('/departments', function (req, res, next) {
+    m.Department.find()
+        .then(depts => {
+            res.json(depts)
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(400);
+            res.send(err);
+        })
+});
 
 module.exports = router;
